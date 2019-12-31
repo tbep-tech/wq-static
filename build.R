a14b1aaaaf9d7fe367688466d4877098ebfccb5d
fawda123 <mbafs2012@gmail.com>
2019-12-31 16:16:23 -0500

    build file updates maxyr in separate rnw files

--- build.R
+++ build.R
@@ -1,9 +1,20 @@
 
-# knit front, convert to pdf
+# put current year here
+maxyr <- 2019
+
+# input maxyr into wq1, knit, convert to pdf
+wq1 <- readLines('wq1.Rnw')
+torepl <- grepl('^maxyr', wq1)
+wq1[torepl] <- paste0('maxyr <- ', maxyr)
+writeLines(wq1, 'wq1.Rnw')
 knitr::knit('wq1.Rnw', 'wq1.tex')
 tinytex::pdflatex('wq1.tex')
 
-# knit back, convert to pdf
+# input maxyr into wq2, knit, convert to pdf
+wq2 <- readLines('wq2.Rnw')
+torepl <- grepl('^maxyr', wq2)
+wq2[torepl] <- paste0('maxyr <- ', maxyr)
+writeLines(wq2, 'wq2.Rnw')
 knitr::knit('wq2.Rnw', 'wq2.tex')
 tinytex::pdflatex('wq2.tex')
 
